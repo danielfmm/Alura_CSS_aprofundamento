0ed567954ee23a0dbab0e133ed8eee1ba352d091
Daniel Ferraz <danielferrazmm@hotmail.com>
2023-03-09 18:56:42 -0300

    Finalizado o curso de tailwind da Alura

---
+++ aluranewsletter/tw.js
@@ -0,0 +1,33 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            screens: {
+                '3xl': '1800px',
+              },
+            colors: {
+                azul: {
+                    claro: '#C5DFFF',
+                    escuro: '#061E3C',
+                    hover: '#1057B0'
+                },
+            },
+            fontFamily: {
+                inter: ['Inter', 'sans-serif']
+            },
+            keyframes: {
+                sino_kf: {
+                    '0%, 100%': {
+                        transform: 'rotate(-10deg)'
+                    },
+                    '50%': {
+                        transform: 'rotate(10deg)'
+                    }
+                }
+            },
+            animation: {
+                sino: 'sino_kf 300ms ease-in-out infinite'
+            }
+
+        }
+    }
+}
